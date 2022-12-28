cbff6e0ba028cf6f2d50fe5ee470900048350dec
Dongho Gang <gongdongho12@gmail.com>
2022-12-29 01:52:52 +0900

    refactor: 항목 로깅 처리 완료

--- src/containers/TodoList/TodoCheck/TodoCheck.tsx
+++ src/containers/TodoList/TodoCheck/TodoCheck.tsx
@@ -12,7 +12,7 @@ interface ITodoCheckProps {
 }
 
 const TodoCheck: FunctionComponent<ITodoCheckProps> = (props) => {
-  const { id, data, ...rest } = props
+  const { id, data, userId, ...rest } = props
   const { title, check }: { title: string, check: boolean } = JSON.parse(data) as any
   const [checked, setCheckComplete] = useState<boolean>(check)
   const navigate = useNavigate();
@@ -23,20 +23,28 @@ const TodoCheck: FunctionComponent<ITodoCheckProps> = (props) => {
     setCheckComplete(checked)
     userSave.editUserSave(id, {
       id,
+      userId,
       data: JSON.stringify({ title, check: checked }),
       private: true,
       ...rest
     })
       .then((res) => {
         console.log('complete', res.data)
+        return userSave.addUserSave({
+          dataKey: "log_task",
+          data: `${userId}님이 ${title} 을(를) 완료했습니다!`,
+          private: false,
+        })
+      })
+      .then(() => {
         navigate('/task')
       })
-  }, [id, navigate, rest, title]);
+  }, [id, navigate, rest, title, userId]);
 
   return <Checkbox onChange={onChange} onClick={(e) => {
     e.preventDefault()
     e.stopPropagation()
-  }} defaultChecked={check}><span style={checked ? { textDecoration: 'line-through', color: '#eeeeee', fontSize: 20 } : { fontSize: 20 }}>{title}</span></Checkbox>;
+  }} defaultChecked={check}><span style={checked ? { textDecoration: 'line-through', color: '#cccccc', fontSize: 20 } : { fontSize: 20 }}>{title}</span></Checkbox>;
 };
 
 export default TodoCheck;
